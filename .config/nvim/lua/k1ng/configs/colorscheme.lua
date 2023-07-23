812fb636b424b21aecb710153c8fe74e6970f42a
Asaduzzaman Pavel <contact@iampavel.dev>
2023-07-23 10:21:17 +0600

    fixed cmp padding

--- .config/nvim/lua/k1ng/configs/colorscheme.lua
+++ .config/nvim/lua/k1ng/configs/colorscheme.lua
@@ -1,5 +1,1 @@
 # cmp
-
-local set_hl = vim.api.nvim_set_hl
-
-set_hl(0, 'CmpMenu', { fg = '#939ab7', bg = '#303347' })
--- .config/nvim/lua/k1ng/lsp/utils.lua
+++ .config/nvim/lua/k1ng/lsp/utils.lua
@@ -1,7 +1,10 @@
 local M = {}
 local icons = require("k1ng.configs.icons").kinds
 
+
 function M.cmp_formatter(min, max, ellipsis_char)
+  local ellipsis_len = string.len(ellipsis_char)
+  local ellipsis_padding = string.rep(' ', ellipsis_len)
   return function(_, item)
     --@label string|number
     local label = item.abbr
@@ -9,10 +12,9 @@ function M.cmp_formatter(min, max, ellipsis_char)
     if string.len(item.abbr) < min then
       local padding = string.rep(' ', min - string.len(item.abbr))
       item.abbr = item.abbr .. padding
-      item.abbr = item.abbr .. ' ' .. '(' .. item.kind .. ')'
+      item.abbr = item.abbr .. ' ' .. ellipsis_padding .. '(' .. item.kind .. ')'
     elseif string.len(item.abbr) > max then
-      item.abbr = vim.fn.strcharpart(item.abbr, 0, max) .. ellipsis_char
-      item.abbr = item.abbr .. ' ' .. '(' .. item.kind .. ')'
+      item.abbr = vim.fn.strcharpart(item.abbr, 0, max) .. ellipsis_char .. ' ' .. '(' .. item.kind .. ')'
     end
 
     if icons[item.kind] then
